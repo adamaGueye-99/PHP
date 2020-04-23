a5b7284f2c139a53b132c54808699282dfed474a
adamaGueye-99 <62160075+adamaGueye-99@users.noreply.github.com>
2020-04-23 00:09:47 +0000

    Update validation.php

--- validation.php
+++ validation.php
@@ -42,7 +42,6 @@ Longueur> Largeur
 //compare()
 //Nbre1=>plus grand
 //Nbre2=>plus petit
-
 function compare($nbre1,$nbre2,$errorMessage="Longueur doit etre superieure a la largeur"){
     $result1=is_positif($nbre1);
     $result2=is_positif($nbre2);
@@ -63,8 +62,8 @@ function compare($nbre1,$nbre2,$errorMessage="Longueur doit etre superieure a la
     return $error;
     
 }
-
-function is_Empty($nbre,$sms=null){
+ 
+function is_Empty($nbre,$sms=null
     if(empty($nbre)){
         if($sms==null){
             $sms="Le nombre est obligatoire";
@@ -74,7 +73,32 @@ function is_Empty($nbre,$sms=null){
         return true;
     }
 }
+public function  is_empty($nbre,$key,$sms=null){
+    if(empty($nbre)){
+        if($sms==null){
+            $sms="Le Nombre  est Obligatoire";
+        }
+        $this->errors[$key]= $sms;
+        }
+    }
+//Expressions Régulières
+    public function  is_email($valeur,$key,$sms=null){
+      if(!filter_var($valeur,FILTER_VALIDATE_EMAIL)){
+          if($sms==nul){
+              $sms="c'est pas un mail";
+          }
+          $this->errors[$key]= $sms;
+      }
+    }
 
-
-
+    //9chiffres , commence par 77,78,75,76,70
+    public function  is_telephone($valeur,$key,$sms=null){
+    if(!preg_match("#[7][5-8][- \.?]?[0-9][0-9][0-9][- \.?]?([0-9][0-9][- \.?]?){2}#",$valeur)){
+        if($sms==null){
+            $sms="Le Numéro de telephone n'est pas correcte";
+        }
+        $this->errors[$key]=$sms;
+    }
+    }
+}
 ?>
